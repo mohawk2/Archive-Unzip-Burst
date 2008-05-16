6786b3a98b2fdb8c4a55556fa0706f65edd0f296
Steffen Müller <smueller@cpan.org>
2008-05-16 12:03:35 +0000

    Import of SMUELLER/Archive-Unzip-Burst-0.02_02 from CPAN.
    gitpan-cpan-distribution: Archive-Unzip-Burst
    gitpan-cpan-version:      0.02_02
    gitpan-cpan-path:         SMUELLER/Archive-Unzip-Burst-0.02_02.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     developer

--- lib/Archive/Unzip/Burst.pm
+++ lib/Archive/Unzip/Burst.pm
@@ -86,7 +86,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw();
 
-our $VERSION = '0.02_01';
+our $VERSION = '0.02_02';
 
 require XSLoader;
 XSLoader::load('Archive::Unzip::Burst', $VERSION);
